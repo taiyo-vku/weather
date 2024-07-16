094e87483a3567fddeba963f4c186c3a223ec228
Taiyo Kun <hackerlord10x@gmail.com>
2024-07-17 06:52:09 +0700

    welcome screen

--- lib/views/welcome.dart
+++ lib/views/welcome.dart
@@ -1,14 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:weather/utils/constants/app_colors.dart';
 
-class Welcome extends StatelessWidget {
+import '../models/city.dart';
+
+class Welcome extends StatefulWidget {
   const Welcome({super.key});
 
   @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
   Widget build(BuildContext context) {
+    List<City> cities =
+        City.citiesList.where((city) => city.isDefault == true).toList();
+
+    List<City> selectedCities = City.getSelectedCities();
+
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Welcome'),
+        automaticallyImplyLeading: false,
+        backgroundColor: AppColors.sencond,
+        title: Text('${selectedCities.length} selected'),
+      ),
+      body: ListView.builder(
+        itemCount: cities.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: const EdgeInsets.only(left: 10, right: 10, top: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            height: size.height * .08,
+            width: size.width,
+            decoration: BoxDecoration(
+              border: cities[index].isSelected == true
+                  ? Border.all(
+                      color: AppColors.sencond.withOpacity(.6), width: 2)
+                  : Border.all(color: Colors.white),
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.primary.withOpacity(.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      cities[index].isSelected = !cities[index].isSelected;
+                    });
+                  },
+                  child: Image.asset(
+                    cities[index].isSelected == true
+                        ? 'assets/checked.png'
+                        : 'assets/unchecked.png',
+                    width: 30,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Text(cities[index].city,
+                    style: Theme.of(context).textTheme.bodyLarge)
+              ],
+            ),
+          );
+        },
       ),
     );
   }
